6b03e8845e23b4eba5e0f0c684f7d4202648b165
Clément PRUDHOMME <clement.prudhomme@viacesi.fr>
2020-05-12 16:31:51 +0200

    Ajout du projet de TU

---
+++ TotoApp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TotoApp;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var calc = new ConverterLibrary();
+
+            //Act
+            var result = calc.GetInt("42");
+
+            //Assert
+            Assert.AreEqual(42, result);
+        }
+    }
+}
